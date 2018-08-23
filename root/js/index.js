ae8ce7ba776a02082299f92dd8cb0d8f9e6640a0
ChristopherGajete <galileo-034@hotmail.fr>
2018-08-23 14:54:42 +0200

    update carousel init

--- root/js/index.js
+++ root/js/index.js
@@ -7,3 +7,36 @@ $(document).ready(function(){
         cptI.toggleClass("fa-angle-down");
     });
 });
+
+var $item = $('.carousel-item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image' : 'url(' + $src + ')',
+        'background-color' : $color
+    });
+    $(this).remove();
+});
+
+$(window).on('resize', function (){
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
+});
+$('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
+});
+
+
+
